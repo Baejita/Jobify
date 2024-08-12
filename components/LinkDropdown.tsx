f6183c6890c9f54796ee9b3e308054706c452013
Baejita <homokoro@gmail.com>
2024-08-12 18:53:51 +0700

    dropdownMenu

--- components/LinkDropdown.tsx
+++ components/LinkDropdown.tsx
@@ -1,9 +1,45 @@
-
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+import { Button } from "./ui/button"
+import { AlignLeft } from "lucide-react"
+import Link from "next/link"
+import links from "@/app/utils/links"
 
 const LinkDropDrown = () => {
   return (
-    <h2 className="text-4xl">LinkDropDrown</h2>
+    <DropdownMenu >
+      <DropdownMenuTrigger asChild className='lg:hidden'>
+        <Button variant='outline' size='icon'>
+          <AlignLeft />
+
+          <span className='sr-only'>Toggle links</span>
+        </Button>
+      </DropdownMenuTrigger>
+<DropdownMenuContent
+        className='w-52 lg:hidden '
+        align='start'
+        sideOffset={25}
+      >
+        {links.map((link) => {
+          return (
+            <DropdownMenuItem key={link.href}>
+              <Link href={link.href} className='flex items-center gap-x-2 '>
+                {link.icon} <span className='capitalize'>{link.label}</span>
+              </Link>
+            </DropdownMenuItem>
+          );
+        })}
+      </DropdownMenuContent>
+
+    </DropdownMenu>
   )
 }
 
 export default LinkDropDrown
+
